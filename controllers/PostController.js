f80a0829ee3386bd97e0806c053991109dea5dd0
Vanesa <vaneebg@gmail.com>
2022-05-31 16:18:28 +0200

    hecho modelo post y endpoint crear post

---
+++ controllers/PostController.js
@@ -0,0 +1,21 @@
+const Post = require("../models/Post");
+const PostController = {
+    async create(req, res) {
+        try {
+            const post = await Post.create(req.body)
+            res.status(201).send(post)
+        } catch (error) {
+            console.error(error)
+            res.status(500).send({ message: 'Ha habido un problema al crear el Post' })
+        }
+    },
+    async getAll(req, res) {
+        try {
+            const posts = await Post.find()
+            res.send(posts)
+        } catch (error) {
+            console.error(error);
+        }
+    },
+}
+module.exports = PostController;
--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const PORT = 8080;
 const { dbConnection } = require("./config/config")
 app.use(express.json())
 dbConnection()
+
+app.use('/posts', require('./routes/posts'));
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
