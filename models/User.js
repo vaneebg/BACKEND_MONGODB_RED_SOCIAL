65d441d21208795c7ffe4db9e0dc529fe3f4ffc2
Vanesa <vaneebg@gmail.com>
2022-05-31 16:36:59 +0200

    creado modelo user

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+}, { timestamps: true });
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
