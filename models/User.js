729cfc96b64f5c1c1883a48d648cbecb3058b32b
Vanesa <vaneebg@gmail.com>
2022-06-02 10:36:03 +0200

    implementado no enviar tokens ni contras en endpoints

--- models/User.js
+++ models/User.js
@@ -27,5 +27,12 @@ const UserSchema = new mongoose.Schema({
     role: String,
 }, { timestamps: true });
 
+UserSchema.methods.toJSON = function() {
+    const user = this._doc;
+    delete user.tokens;
+    delete user.password;
+    return user;
+}
+
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
