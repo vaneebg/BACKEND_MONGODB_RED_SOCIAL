aa6633d3c5085e3ded1508b382a96bf690fc45b6
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-02 21:46:02 +0200

    populate en like y dislike

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -79,10 +79,12 @@ const PostController = {
     async like(req, res) {
         try {
             const existPost = await Post.findById(req.params._id)
+           
             if (!existPost.likes.includes(req.user._id)) {
                 const post = await Post.findByIdAndUpdate(
                     req.params._id, { $push: { likes: req.user._id } }, { new: true }
-                );
+                ).populate({ path: 'userId', select: 'username email image' })
+                .populate({ path: 'commentsId', populate: { path: 'userId', select: 'username image' } })
 
                 await User.findByIdAndUpdate(
                     req.user._id, { $push: { favList: req.params._id } }, { new: true }
@@ -103,7 +105,8 @@ const PostController = {
             if (existPost.likes.includes(req.user._id)) {
               const post=  await Post.findByIdAndUpdate(
                     req.params._id, { $pull: { likes: req.user._id } }, { new: true }
-                );
+                ).populate({ path: 'userId', select: 'username email image' })
+                .populate({ path: 'commentsId', populate: { path: 'userId', select: 'username image' } })
                 await User.findByIdAndUpdate(
                     req.user._id, { $pull: { favList: req.params._id } }, { new: true }
                 );
